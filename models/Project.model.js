8b08a4e25867efe439b83568c360bd54b71a9cc5
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-12-03 20:44:30 +0100

    add project preferences, oneTimeId as random generate id to share with students when preferencesOpen

--- models/Project.model.js
+++ models/Project.model.js
@@ -3,34 +3,73 @@ const { Schema, model } = require("mongoose");
 const projectSchema = new Schema(
   {
     projectType: {
-        type: String,
-        required: [true, "Type of project is required."],
-        enum: ["Project 2", "Final Project", "Mini Project",]
+      type: String,
+      required: [true, "Type of project is required."],
+      enum: [
+        "Project 2",
+        "Final Project",
+        "Mini Project Mod 2",
+        "Mini Project Mod 3",
+      ],
     },
 
     preferencesNumber: {
-        type: Number,
-        required: [true, "Number of preferences is required."],
-        min: 5,
-        max: 9,
+      type: Number,
+      required: [true, "Number of preferences is required."],
+      min: 5,
+      max: 9,
     },
 
     blockedNumber: {
-        type: Number,
-        default: 3,
-        max: 4,
+      type: Number,
+      default: 3,
+      max: 4,
+    },
+
+    oneTimeId: {
+      uuId: {
+        type: String,
+      },
+      expirationDate: {
+        type: Date,
+      },
     },
 
     partecipants: {
-        type: [Schema.Types.ObjectId],
-        ref: "Student",
-    }
+      type: [Schema.Types.ObjectId],
+      ref: "Student",
+    },
+
+    preferencesOpen: {
+      type: Boolean,
+      default: false
+    },
+
+    partecipantsPreference: [
+      {
+        student: {
+          type: Schema.Types.ObjectId,
+          ref: "Student",
+        },
+
+        preferences: {
+          type: [String],
+          lowercase: true,
+          required: [true, "Preferences are required."],
+        },
+
+        blocked: {
+          type: [String],
+          lowercase: true,
+          default: ["NO PREFERENCES"],
+        },
+      },
+    ],
   },
 
   {
     timestamps: true,
   }
-
 );
 
 module.exports = model("Project", projectSchema);
