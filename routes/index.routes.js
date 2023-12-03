e6cbf6da578606094fd1f09616533b47926cb566
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-12-03 20:57:05 +0100

    create route that return a random -code- used to validate students

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,8 +1,15 @@
 const express = require("express");
 const router = express.Router();
+const { isAuthenticated } = require("../middleware/jwt.middleware");
+const getOneTimeProjectCode = require("../utils/getOneTimeProjectCode");
 
 router.get("/", (req, res, next) => {
   res.json("All good in here");
+});
+
+router.post("/project-code", isAuthenticated, async (req, res, next) => {
+  const code = getOneTimeProjectCode(req.body.date);
+  res.json(code);
 });
 
 module.exports = router;
