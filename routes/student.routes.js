2e2b2f4b4124041b00e134015c1b38cd27866bb7
Alessandra Scarpellini <scaalessandra@icloud.com>
2023-12-16 17:25:57 +0100

    create student routes: validation that receive the student name and the oneTime code to access the preferences area, and return jwt token. verifyStudent to validate student token and get payload back

--- routes/student.routes.js
+++ routes/student.routes.js
@@ -5,8 +5,10 @@ const Project = require("../models/Project.model");
 const validatePartecipant = require("../utils/validatePartecipant");
 const validateCode = require("../utils/validateCode");
 const jwt = require("jsonwebtoken");
+const { isAuthenticatedStudent } = require("../middleware/jwt.student.middleware.js");
 
-router.get("/", async (req, res, next) => {
+
+router.get("/validation", async (req, res, next) => {
 
   try {
     const validCode = validateCode(req.query.code)
@@ -17,31 +19,38 @@ router.get("/", async (req, res, next) => {
       return
     }
 
-    const project = await Project.findOne({
-      "oneTimeId.uuId": req.query.code,
-    }).select('-partecipantsPreference').populate({ path: "partecipants", select:'studentName'})
+    const project = await Project.findOne({"oneTimeId.uuId": req.query.code,})
+    .select('-partecipantsPreference').populate({ path: "partecipants", select:'studentName'})
 
     // here check if code expire!!! --> project.oneTimeId.expirationDate
 
   const partecipant = validatePartecipant(project.partecipants, req.query.studentName);
     if(!partecipant){
       res.status(401).json({
-        message: "Your name is not in the list of this project partecipants ",
+        message: "Your name is not in the list of this project's partecipants ",
       });
       return
     }
   const student = await Student.findOne({"studentName": req.query.studentName, "projects": project._id}).select("studentName")
 
+  const removeStudentForPreferences = () => {
+    return project.partecipants.filter((el) => {
+        return el.studentName !== req.query.studentName.toLowerCase();
+      }
+    );
+  };
+
   const payload = {
     validateStudent: student,
-    project
+    project,
+    partecipants: removeStudentForPreferences()
   }
   const studentToken = jwt.sign(payload, process.env.TOKEN_STUDENT, {
     algorithm: "HS256",
     expiresIn: "1h",
   });
 
-  res.status(200).json({ studentToken });
+  res.status(200).json( studentToken );
 
   } catch (err) {
     res.status(500).json({
@@ -51,6 +60,9 @@ router.get("/", async (req, res, next) => {
   }
 });
 
+router.get("/verifyStudent", isAuthenticatedStudent, (req, res, next) => {
+  res.status(200).json(req.payload);
+})
 
 
 module.exports = router;
