ead317eda8810ea7299ff69381b548dbb8f5e0f1
Alessandra Scarpellini <scaalessandra@icloud.com>
2024-02-28 19:52:52 +0100

    add property accessTo

--- models/Cohort.model.js
+++ models/Cohort.model.js
@@ -35,6 +35,10 @@ const cohortSchema = new Schema(
         type: Schema.Types.ObjectId,
         ref: "User",
     },
+    accessTo: {
+      type: [Schema.Types.ObjectId],
+      ref: "User",
+  },
   },
 
   {
